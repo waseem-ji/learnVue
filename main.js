5ce8767768c820fc3a9288aa0ca043ca57f6beaf
waseem-ji <waseem@mozilor.com>
2023-04-25 17:19:28 +0530

    Sibling communication using a shared event instance

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-Vue.component('coupon',{
-    template: '<input placeholder="enteryour coupon code" @blur="onCouponApplied">',
-    methods: {
-        onCouponApplied() {
-            this.$emit("applied")
-        }
+window.Event = new Vue();
+
+Vue.component("coupon", {
+  template:
+    '<input placeholder="enteryour coupon code" @blur="onCouponApplied">',
+  methods: {
+    onCouponApplied() {
+      Event.$emit("applied");
     },
-})
+  },
+});
 
 new Vue({
-    el:"#root",
-    data:{
-        couponApplied :false
-    },
-    methods:{
-        some() {
-           this.couponApplied = true
-        }
-    }
+  el: "#root",
+  data: {
+    couponApplied: false,
+  },
+  created() {
+    Event.$on("applied", () => alert("Yeah , he applied"));
+  },
 });
