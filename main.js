76f6dc559c8bc1c823a7ff0aed40f95887337451
waseem-ji <waseem@mozilor.com>
2023-04-25 12:35:43 +0530

    Recreated a simple article card styled in bulma with vue components

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+Vue.component("message", {
+  props: ["title", "body"],
+
+  data() {
+    return {
+      isVisible: true,
+    };
+  },
+
+  template: `
+  <article class="message" v-show="isVisible">
+  <div class="message-header">
+    <p>{{title}} </p>
+    <button class="delete" aria-label="delete" @click="isVisible=false"></button>
+  </div>
+  <div class="message-body">
+   {{body}}
+  </div>
+</article>
+    `,
+});
+
 Vue.component("task", {
   template: "<li><slot></slot> </li>",
 });
