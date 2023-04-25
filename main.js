482c8f773d5dec27afe7c464e725b22ddfe1d8e7
waseem-ji <waseem@mozilor.com>
2023-04-25 17:44:27 +0530

    Named slots

--- main.js
+++ main.js
@@ -2,14 +2,23 @@ Vue.component("modal", {
   template: `
   <div class="modal is-active">
   <div class="modal-background"></div>
-  <div class="modal-content">
-    <div class="box">
-        <p>
-            <slot></slot>
-        </p>
-    </div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title"><slot name="title"></slot> </p>
+      <button class="delete" aria-label="close"  @click="$emit('close')"></button>
+    </header>
+    <section class="modal-card-body">
+      <p>
+        <slot name="body"></slot>
+      </p>
+    </section>
+    <footer class="modal-card-foot">
+        <slot name="footer">
+            <button class="button is-success">Save changes</button>
+            <button class="button"  @click="$emit('close')">Cancel</button>
+        </slot>
+    </footer>
   </div>
-  <button class="modal-close is-large" aria-label="close" @click="$emit('close')" ></button>
 </div>
     `,
 });
